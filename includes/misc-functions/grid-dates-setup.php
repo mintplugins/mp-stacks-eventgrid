a9f76228b115986ecb319cece82fac499373cfb5
Phil Johnston <johnstonphilip@gmail.com>
2018-06-05 13:12:05 -0400

    Make grid date month translateable with mp_eventgrid_month_name
    Filter added to enable simple month translation: mp_eventgrid_month_name
    
    (also whitespace fixes)

--- includes/misc-functions/grid-dates-setup.php
+++ includes/misc-functions/grid-dates-setup.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 /**
- * This file contains the function which set up the Dates in the Grid. 
+ * This file contains the function which set up the Dates in the Grid.
  *
- * To use this for additional Text Overlays in a grid, duplicate this file 
+ * To use this for additional Text Overlays in a grid, duplicate this file
  * 1. Find and replace "eventgrid" with your plugin's prefix
  * 2. Find and replace "date" with your desired text overlay name
  * 3. Make custom changes to the mp_stacks_eventgrid_date function about what is displayed.
@@ -26,8 +26,8 @@
  * @param    $post_id Int - The ID of the Brick
  * @return   Array - All of the placement optons needed for Date
  */
-function mp_stacks_eventgrid_date_meta_options( $items_array ){		
-	
+function mp_stacks_eventgrid_date_meta_options( $items_array ){
+
 	//Date Settings
 	$new_fields = array(
 		//Date
@@ -79,7 +79,7 @@ function mp_stacks_eventgrid_date_meta_options( $items_array ){
 			'field_value' => '13',
 			'field_showhider' => 'eventgrid_date_settings',
 		),
-		
+
 		'eventgrid_date_google_font' => array(
 			'field_id'			=> 'eventgrid_date_google_font',
 			'field_title' 	=> __( 'Google Font Name', 'mp_stacks'),
@@ -94,20 +94,20 @@ function mp_stacks_eventgrid_date_meta_options( $items_array ){
 			'field_title' 	=> __( 'Font Weight/Style', 'mp_stacks'),
 			'field_description' 	=> 'Set the weight of this font (If available for your chosen font)',
 			'field_type' 	=> 'select',
-			'field_select_values' => array( 
-				'100' => 'Thin', 
-				'200' => 'Extra-Light', 
-				'300' => 'Light', 
-				'400' => 'Normal', 
-				'500' => 'Medium', 
-				'600' => 'Semi-Bold', 
+			'field_select_values' => array(
+				'100' => 'Thin',
+				'200' => 'Extra-Light',
+				'300' => 'Light',
+				'400' => 'Normal',
+				'500' => 'Medium',
+				'600' => 'Semi-Bold',
 				'700' => 'Bold',
-				'900' => 'Ultra-Bold', 
+				'900' => 'Ultra-Bold',
 			),
 			'field_value' => '',
 			'field_showhider' => 'eventgrid_date_settings',
 		),
-		
+
 		'eventgrid_date_spacing' => array(
 			'field_id'			=> 'eventgrid_date_spacing',
 			'field_title' 	=> __( 'Dates\' Spacing', 'mp_stacks_eventgrid'),
@@ -221,7 +221,7 @@ function mp_stacks_eventgrid_date_meta_options( $items_array ){
 		),
 
 	);
-	
+
 	return mp_core_insert_meta_fields( $items_array, $new_fields, 'eventgrid_meta_hook_anchor_2' );
 
 }
@@ -237,14 +237,14 @@ add_filter( 'mp_stacks_eventgrid_items_array', 'mp_stacks_eventgrid_date_meta_op
  * @return   Array - All of the placement optons needed for Date
  */
 function mp_stacks_eventgrid_date_placement_options( $placement_options, $post_id ){
-	
+
 	//Show Post Dates
 	$placement_options['date_show'] = mp_core_get_post_meta($post_id, 'eventgrid_date_show');
 
 	//Dates Placement
 	$placement_options['date_placement'] = mp_core_get_post_meta($post_id, 'eventgrid_date_placement', 'over_image_top_left');
-	
-	return $placement_options;	
+
+	return $placement_options;
 }
 add_filter( 'mp_stacks_eventgrid_placement_options', 'mp_stacks_eventgrid_date_placement_options', 10, 2 );
 
@@ -257,40 +257,40 @@ add_filter( 'mp_stacks_eventgrid_placement_options', 'mp_stacks_eventgrid_date_p
  * @return   $html_output String - A string holding the html for an date in the grid
  */
 function mp_stacks_eventgrid_date( $post_id, $word_limit, $read_more_text, $mp_event_start_date, $mp_event_end_date ){
-		
+
 	$the_start_date = mp_events_format_mp_event_date( $mp_event_start_date );
 	$the_start_time = mp_core_get_post_meta( $post_id, 'event_start_time' );
-	$the_start_date_timestamp = strtotime( $the_start_date );
+	$the_start_date_timestamp = strtotime( $mp_event_start_date );
 	$the_start_date = date( get_option( 'date_format' ), $the_start_date_timestamp );
 	$time_description = mp_core_get_post_meta( $post_id, 'event_time_description', $the_start_time );
-	
+
 	//Add the calendar-style date output (fancy)
 	$the_fancy_date = '
 	<div class="mp-stacks-eventgrid-item-date-holder">
 		<div class="mp-stacks-eventgrid-fancy-date">
 			<figure>
 				<header>
-					<span class="mp-stacks-eventgrid-month">' . date( 'M', $the_start_date_timestamp ) . '</span>
+					<span class="mp-stacks-eventgrid-month">' . apply_filters( 'mp_eventgrid_month_name', date( 'M', $the_start_date_timestamp ) ) . '</span>
 					<span class="mp-stacks-eventgrid-year">' . date( 'Y', $the_start_date_timestamp ) . '</span>
 				</header>
 				<section>' . date( 'j', $the_start_date_timestamp ) . '</section>
 			</figure>';
-	//Add the time to the fancy date	
-	$the_fancy_date .=		
+	//Add the time to the fancy date
+	$the_fancy_date .=
 			'<div class="mp-stacks-eventgrid-fancy-time">
 				<section>' . $time_description . '</section>
 			</div>';
-	
-	//Finish the date output		
+
+	//Finish the date output
 	$the_fancy_date .= '</div>
 	</div>';
 
-	$the_text_only_date = mp_stacks_grid_highlight_text_html( array( 
+	$the_text_only_date = mp_stacks_grid_highlight_text_html( array(
 		'class_name' => 'mp-stacks-eventgrid-item-date',
-		'output_string' => $the_start_date . ' ' . $time_description, 
+		'output_string' => $the_start_date . ' ' . $time_description,
 	) );
-	
-	return $the_fancy_date;	
+
+	return $the_fancy_date;
 
 }
 
@@ -303,16 +303,16 @@ function mp_stacks_eventgrid_date( $post_id, $word_limit, $read_more_text, $mp_e
  * @return   $html_output String - A string holding the html for text over a featured image in the grid
  */
 function mp_stacks_eventgrid_date_top_over_callback( $eventgrid_output, $grid_post_id, $options ){
-	
+
 	//If we should show the date over the image
 	if ( strpos( $options['date_placement'], 'over') !== false && strpos( $options['date_placement'], 'top') !== false && $options['date_show']){
-		
+
 		return $eventgrid_output . mp_stacks_eventgrid_date( $grid_post_id, $options['word_limit'], $options['read_more_text'], $options['mp_event_start_date'], $options['mp_event_end_date'] );
 
 	}
-	
+
 	return $eventgrid_output;
-	
+
 }
 add_filter( 'mp_stacks_eventgrid_top_over', 'mp_stacks_eventgrid_date_top_over_callback', 14, 3 );
 
@@ -325,14 +325,14 @@ add_filter( 'mp_stacks_eventgrid_top_over', 'mp_stacks_eventgrid_date_top_over_c
  * @return   $html_output String - A string holding the html for text over a featured image in the grid
  */
 function mp_stacks_eventgrid_date_middle_over_callback( $eventgrid_output, $grid_post_id, $options ){
-	
+
 	//If we should show the date over the image
 	if ( strpos( $options['date_placement'], 'over') !== false && strpos( $options['date_placement'], 'middle') !== false && $options['date_show']){
-		
+
 		return $eventgrid_output . mp_stacks_eventgrid_date( $grid_post_id, $options['word_limit'], $options['read_more_text'], $options['mp_event_start_date'], $options['mp_event_end_date'] );
 
 	}
-	
+
 	return $eventgrid_output;
 }
 add_filter( 'mp_stacks_eventgrid_middle_over', 'mp_stacks_eventgrid_date_middle_over_callback', 14, 3 );
@@ -346,16 +346,16 @@ add_filter( 'mp_stacks_eventgrid_middle_over', 'mp_stacks_eventgrid_date_middle_
  * @return   $html_output String - A string holding the html for text over a featured image in the grid
  */
 function mp_stacks_eventgrid_date_bottom_over_callback( $eventgrid_output, $grid_post_id, $options ){
-	
+
 	//If we should show the date over the image
 	if ( strpos( $options['date_placement'], 'over') !== false && strpos( $options['date_placement'], 'bottom') !== false && $options['date_show']){
-		
+
 		return $eventgrid_output . mp_stacks_eventgrid_date( $grid_post_id, $options['word_limit'], $options['read_more_text'], $options['mp_event_start_date'], $options['mp_event_end_date'] );
 
 	}
-	
+
 	return $eventgrid_output;
-	
+
 }
 add_filter( 'mp_stacks_eventgrid_bottom_over', 'mp_stacks_eventgrid_date_bottom_over_callback', 14, 3 );
 
@@ -368,19 +368,19 @@ add_filter( 'mp_stacks_eventgrid_bottom_over', 'mp_stacks_eventgrid_date_bottom_
  * @return   $html_output String - A string holding the html for text over a featured image in the grid
  */
 function mp_stacks_eventgrid_date_below_over_callback( $eventgrid_output, $grid_post_id, $options ){
-	
+
 	//If we should show the date below the image
 	if ( strpos( $options['date_placement'], 'below') !== false && $options['date_show']){
-		
-		$date_html_output = '<a href="' . get_permalink() . '" class="mp-stacks-eventgrid-date-link">';	
+
+		$date_html_output = '<a href="' . get_permalink() . '" class="mp-stacks-eventgrid-date-link">';
 			$date_html_output .= mp_stacks_eventgrid_date( $grid_post_id, $options['word_limit'], $options['read_more_text'], $options['mp_event_start_date'], $options['mp_event_end_date'] );
 		$date_html_output .= '</a>';
-		
+
 		return $eventgrid_output . $date_html_output;
 	}
-	
+
 	return $eventgrid_output;
-	
+
 }
 add_filter( 'mp_stacks_eventgrid_below', 'mp_stacks_eventgrid_date_below_over_callback', 14, 3 );
 
@@ -395,18 +395,18 @@ add_filter( 'mp_stacks_eventgrid_below', 'mp_stacks_eventgrid_date_below_over_ca
  * @return   $new_grid_output - the existing grid output with additional thigns added by this function.
  */
 function mp_stacks_eventgrid_date_animation_js( $existing_filter_output, $post_id, $meta_prefix ){
-	
+
 	if ( $meta_prefix != 'eventgrid' ){
-		return $existing_filter_output;	
+		return $existing_filter_output;
 	}
-	
+
 	//Get JS output to animate the dates on mouse over and out
-	$date_animation_js = mp_core_js_mouse_over_animate_child( '#mp-brick-' . $post_id . ' .mp-stacks-grid-item', '.mp-stacks-eventgrid-item-date-holder', mp_core_get_post_meta( $post_id, 'eventgrid_date_animation_keyframes', array() ), true, true, 'mp-brick-' . $post_id ); 
-	
+	$date_animation_js = mp_core_js_mouse_over_animate_child( '#mp-brick-' . $post_id . ' .mp-stacks-grid-item', '.mp-stacks-eventgrid-item-date-holder', mp_core_get_post_meta( $post_id, 'eventgrid_date_animation_keyframes', array() ), true, true, 'mp-brick-' . $post_id );
+
 	return $existing_filter_output .= $date_animation_js;
 }
 add_filter( 'mp_stacks_grid_js', 'mp_stacks_eventgrid_date_animation_js', 10, 3 );
-		
+
 /**
  * Add the CSS for the date to EventGrid's CSS
  *
@@ -416,7 +416,7 @@ add_filter( 'mp_stacks_grid_js', 'mp_stacks_eventgrid_date_animation_js', 10, 3 
  * @return   $css_output String - The incoming CSS with our new CSS for the date appended.
  */
 function mp_stacks_eventgrid_date_css( $css_output, $post_id ){
-	
+
 	$date_css_defaults = array(
 		'color' => '#000',
 		'size' => 13,
@@ -442,35 +442,35 @@ add_filter('mp_stacks_eventgrid_css', 'mp_stacks_eventgrid_date_css', 10, 2);
  * @return   $css_output          String - A string holding the css the brick
  */
 function mp_stacks_eventgrid_date_google_font( $css_output, $post_id, $first_content_type, $second_content_type ){
-	
+
 	if ( $first_content_type != 'eventgrid' && $second_content_type != 'eventgrid' ){
-		return $css_output;	
+		return $css_output;
 	}
-	
+
 	global $mp_stacks_footer_inline_css, $mp_core_font_families;
-	
+
 	//Default settings for the MP Core Google Font Class
 	$mp_core_google_font_args = array( 'echo_google_font_css' => false, 'wrap_in_style_tags' => false );
-	
+
 	$eventgrid_date_googlefont = mp_core_get_post_meta( $post_id, 'eventgrid_date_google_font' );
 	$eventgrid_date_googlefontweight = mp_core_get_post_meta( $post_id, 'eventgrid_date_google_font_weight_style' );
-	
+
 	//If a font name has been entered
 	if ( !empty( $eventgrid_date_googlefont ) ){
-		
+
 		//Check if a font extra (weight) has been selected and add it if so.
 		$eventgrid_date_googlefontweight = isset($eventgrid_date_googlefontweight) && !empty( $eventgrid_date_googlefontweight ) ? ':' . $eventgrid_date_googlefontweight : NULL;
 		$eventgrid_date_googlefont = $eventgrid_date_googlefont . $eventgrid_date_googlefontweight;
-	
+
 		//Load the Google Font using the Google Font Class in MP Core
 		new MP_CORE_Font( $eventgrid_date_googlefont, $eventgrid_date_googlefont, $mp_core_google_font_args );
 		$mp_stacks_footer_inline_css[$eventgrid_date_googlefont] = $mp_core_font_families[$eventgrid_date_googlefont];
-		
+
 		//Return the incoming css string plus css to apply this font family to all paragraph tags
 		$css_output .=  '#mp-brick-' . $post_id . ' .mp-stacks-eventgrid-item-date, #mp-brick-' . $post_id . ' .mp-stacks-eventgrid-item-date * { font-family: \'' . $eventgrid_date_googlefont . '\';}';
-	
+
 	}
-	
-	return $css_output;	
+
+	return $css_output;
 }
-add_filter('mp_brick_additional_css', 'mp_stacks_eventgrid_date_google_font', 10, 4);	
+add_filter('mp_brick_additional_css', 'mp_stacks_eventgrid_date_google_font', 10, 4);
